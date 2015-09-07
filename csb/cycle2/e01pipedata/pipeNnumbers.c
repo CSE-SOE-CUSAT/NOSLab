bfd3beb9b29f537643a9dbf94122a9a442f74671
CSBDude <abc@abc.com>
2015-09-07 11:55:04 +0530

    still in process

---
+++ csb/cycle2/e01pipedata/pipeNnumbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+    int childpid,a,n,pd[2],i,buffer1[50],buffer2[50];
+    a = pipe(pd);
+    if(a == -1)
+    {
+       fprintf(stderr, "Pipe creation Failed.\n");
+       exit(1);
+    }
+childpid = fork();
+    switch(childpid){
+       case -1:perror("Error creating child");
+          exit(1);
+       case 0: n=read(pd[0],buffer2,sizeof(buffer2));
+          printf("Inside child, pid: %d\n", getpid());
+          n=n/sizeof(int);
+	  printf("Numbers recieved are :- \n");
+          for(i=0;i<n;i++)
+             printf("%d\n",buffer2[i]);
+          exit(0);
+          break;
+       default: printf("Inside parent, pid: %d\n", getpid());
+          printf("No of input? :\t");
+          scanf("%d",&n);
+	  printf("Enter elements 1 by 1 \n");
+          for(i=0;i<n;i++){
+             scanf("%d",&buffer1[i]);
+          }
+          write(pd[1],buffer1,n*sizeof(int));
+          waitpid(childpid,NULL,0);
+          break;
+   }
+   close(pd[0]);
+    close(pd[1]);
+    return 0;
+}
+
---
+++ csb/cycle2/e02pipestring/pipestring.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+   int pid,a,n,p[2],i,vv;
+   char *str1[100],*str2[100];
+   for(i=0;i<100;i++){
+   str1[i]=(char *)malloc(sizeof(char) * (101));
+   str2[i]=(char *)malloc(sizeof(char) * (101));
+   }
+   printf("Enter the limit:- ");
+   scanf("%d",&n);
+   a = pipe(p); //Unidirectional pipe
+   if(a == -1)
+   {
+       fprintf(stderr, "Pipe Failed.\n");
+       return EXIT_FAILURE;
+    }
+    pid = fork();
+   switch(pid){
+       case -1:perror("main: fork"); //error encountered
+          exit(1);
+       case 0: read(p[0],str2,sizeof(str2)); //read from pipe
+          printf("In child process (ID: %d)\n", pid);
+          for(i=0;i<n;i++)
+             printf("String %d:- %s\n",i+1,str2[i]);
+          exit(1);
+          break;
+       default: printf("In parent process (ID: %d)\n", pid);
+          for(i=0;i<n;i++){
+             printf("Enter the string %d:- ",i+1);
+             scanf("%s",str1[i]);
+          }
+          write(p[1],str1,sizeof(str1)); //write to pipe
+          waitpid(pid,NULL,0); //suspend parent process till the state of child process is changed
+          break;
+    }
+    close(p[0]);
+    close(p[1]);
+    return 0;
+}
+
