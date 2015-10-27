15dd3c22549b58432e753407d834bf01e5781bb4
CSA Dude <csadude@gmail.com>
2015-10-27 18:39:47 +0530

    rmer

--- noslab/unsorted/athulraj/filesending/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-host = socket.gethostname()                           
-port = 1743
-s.connect((host, port))                               
-f = open('a.png', 'r')
-s.send(f.read())
-s.close()
-
