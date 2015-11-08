c40b02631ae763b7dbe15a56fc0a0abeb80a6b4c
CSB Dude <zanec@trickmail.net>
2015-11-08 19:40:06 +0530

    Moodle resources

---
+++ csb/cycle2/e03pipeprime/pipeodd.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<sys/types.h>
+int odd(int);
+int main(){
+    int pid,a,n,p[2],p2[2],i,buffr1[100],buffr2[100],n2=0;
+    a = pipe(p);
+    if(a == -1)
+    {
+       fprintf(stderr, "Pipe Failed.\n");
+       return EXIT_FAILURE;
+    }
+    a = pipe(p2);
+    if(a == -1)
+    {
+       fprintf(stderr, "Pipe Failed.\n");
+       return EXIT_FAILURE;
+    }
+    pid = fork();
+    switch(pid){
+       case -1:perror("main: fork");
+          exit(1);
+       case 0: n=read(p[0],buffr2,sizeof(buffr2));
+          printf("In child process (ID: %d)\n",getpid());
+          n=n/sizeof(int);
+          for(i=0;i<n;i++){
+             if(odd(buffr2[i])){
+	     	
+                buffr1[n2]=buffr2[i];
+                n2++;
+             }
+          }
+          write(p2[1],buffr1,n2*sizeof(int));
+          exit(1);
+	
+          break;
+       default: printf("In parent process (ID: %d)\n", getpid());
+          printf("Number of elements:- ");
+          scanf("%d",&n); 
+	  printf("Enter the elements\n");
+          for(i=0;i<n;i++){
+             scanf("%d",&buffr1[i]);
+          }
+          write(p[1],buffr1,n*sizeof(int));
+          waitpid(pid,NULL,0);
+          printf("In parent process (ID: %d)\n", getpid());
+          n2=read(p2[0],buffr2,sizeof(buffr2));
+          n2=n2/sizeof(int);
+          for(i=0;i<n2;i++)
+             printf("%d is odd\n",buffr2[i]);
+  
+	break;
+   }
+   close(p[0]);
+}
+int odd(int n){
+	
+	if(n%2 != 0)
+		return 1;
+	else
+		return 0;
+	}
